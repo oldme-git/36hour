f371347a164d34e74c0ca5c87c8d629e5a7387d3
oldme <tyyn1022@gmail.com>
2024-03-28 16:13:32 +0800

    初始化demo，后续所有的微服务应该从此获取

---
+++ user/internal/cmd/cmd.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
+	"github.com/gogf/gf/v2/os/gcmd"
+	"google.golang.org/grpc"
+	"user/internal/controller/user"
+)
+
+var (
+	Main = gcmd.Command{
+		Name:  "main",
+		Usage: "main",
+		Brief: "user grpc service",
+		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
+			c := grpcx.Server.NewConfig()
+			c.Options = append(c.Options, []grpc.ServerOption{
+				grpcx.Server.ChainUnary(
+					grpcx.Server.UnaryValidate,
+				)}...,
+			)
+			s := grpcx.Server.New(c)
+			user.Register(s)
+			s.Run()
+			return nil
+		},
+	}
+)
