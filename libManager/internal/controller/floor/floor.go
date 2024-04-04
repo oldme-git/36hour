b5851ed2de3a16fa97a26075d72bdec60ee491af
oldme <tyyn1022@gmail.com>
2024-04-04 15:00:56 +0800

    proto 文件

---
+++ libManager/internal/controller/floor/floor.go
@@ -0,0 +1,38 @@
+package floor
+
+import (
+	"context"
+	v1 "libManager/api/lib/v1"
+
+	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
+	"github.com/gogf/gf/v2/errors/gcode"
+	"github.com/gogf/gf/v2/errors/gerror"
+)
+
+type Controller struct {
+	v1.UnimplementedLibServer
+}
+
+func Register(s *grpcx.GrpcServer) {
+	v1.RegisterLibServer(s.Server, &Controller{})
+}
+
+func (*Controller) Create(ctx context.Context, req *v1.CreateReq) (res *v1.CreateRes, err error) {
+	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+}
+
+func (*Controller) GetOne(ctx context.Context, req *v1.GetOneReq) (res *v1.GetOneRes, err error) {
+	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+}
+
+func (*Controller) GetList(ctx context.Context, req *v1.GetListReq) (res *v1.GetListRes, err error) {
+	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+}
+
+func (*Controller) Update(ctx context.Context, req *v1.UpdateReq) (res *v1.UpdateRes, err error) {
+	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+}
+
+func (*Controller) Delete(ctx context.Context, req *v1.DeleteReq) (res *v1.DeleteRes, err error) {
+	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+}
