9d0fbd1b858212435652814d02d0b695c588b864
oldme <tyyn1022@gmail.com>
2024-04-24 16:22:43 +0800

    seat/controller: 完成layout控制器

--- seat/internal/controller/layout/layout.go
+++ seat/internal/controller/layout/layout.go
@@ -2,11 +2,15 @@ package layout
 
 import (
 	"context"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
 	v1 "seat/api/layout/v1"
+	"seat/api/pbentity"
+	"seat/internal/model"
+	"seat/internal/model/entity"
+	"seat/internal/service"
 
 	"github.com/gogf/gf/contrib/rpc/grpcx/v2"
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
 )
 
 type Controller struct {
@@ -18,21 +22,132 @@ func Register(s *grpcx.GrpcServer) {
 }
 
 func (*Controller) Create(ctx context.Context, req *v1.CreateReq) (res *v1.CreateRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	var policyLid int
+
+	if req.PolicyInfo != "" {
+		// 保存自己的策略信息
+		policyLid, err = service.PolicyLayout().Create(ctx, &entity.PolicyLayout{
+			Info: req.PolicyInfo,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	id, err := service.Layout().Create(ctx, &entity.Layout{
+		LocationId: int(req.LocationId),
+		PolicyCId:  int(req.PolicyCId),
+		PolicyLId:  policyLid,
+		LayoutName: req.LayoutName,
+		Map:        req.Map,
+		Status:     int(req.Status),
+		Sort:       int(req.Sort),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CreateRes{Id: int32(id)}, nil
 }
 
 func (*Controller) GetOne(ctx context.Context, req *v1.GetOneReq) (res *v1.GetOneRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	layout, err := service.Layout().GetOne(ctx, int(req.Id))
+	if err != nil {
+		return nil, err
+	}
+	// 获取运行中的策略
+	policyInfo, err := service.Layout().GetRuntimePolicy(ctx, layout)
+	if err != nil {
+		return nil, err
+	}
+
+	return &v1.GetOneRes{
+		Layout: &pbentity.Layout{
+			Id:         int32(layout.Id),
+			LocationId: int32(layout.LocationId),
+			PolicyCId:  int32(layout.PolicyCId),
+			PolicyLId:  int32(layout.PolicyLId),
+			LayoutName: layout.LayoutName,
+			Map:        layout.Map,
+			Status:     int32(layout.Status),
+			Sort:       int32(layout.Sort),
+			Seats:      int32(layout.Seats),
+			CreatedAt:  timestamppb.New(layout.CreatedAt.Time),
+			UpdatedAt:  timestamppb.New(layout.UpdatedAt.Time),
+		},
+		PolicyInfo: policyInfo,
+	}, nil
 }
 
 func (*Controller) GetList(ctx context.Context, req *v1.GetListReq) (res *v1.GetListRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	layouts, err := service.Layout().GetList(ctx, &model.LayoutSearchCondition{
+		Page:       int(req.Page),
+		PageSize:   int(req.PageSize),
+		LayoutName: req.LayoutName,
+		Status:     int(req.Status),
+		SeatsMin:   int(req.SeatsMin),
+		SeatsMax:   int(req.SeatsMax),
+	})
+	if err != nil {
+		return nil, err
+	}
+	total, err := service.Layout().GetTotal(ctx, &model.LayoutSearchCondition{
+		LayoutName: req.LayoutName,
+		Status:     int(req.Status),
+		SeatsMin:   int(req.SeatsMin),
+		SeatsMax:   int(req.SeatsMax),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]*v1.LayoutList, len(layouts))
+	for k, v := range layouts {
+		list[k] = &v1.LayoutList{
+			Id:         int32(v.Id),
+			LocationId: int32(v.LocationId),
+			// TODO, 需要去libManager中获取LocationName
+			LocationName: "",
+			LayoutName:   v.LayoutName,
+			Status:       int32(v.Status),
+			Sort:         int32(v.Sort),
+			Seats:        int32(v.Seats),
+		}
+	}
+	return &v1.GetListRes{Layouts: list, Total: int32(total)}, nil
 }
 
 func (*Controller) Update(ctx context.Context, req *v1.UpdateReq) (res *v1.UpdateRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	var policyLid int
+
+	if req.PolicyInfo != "" {
+		// TODO 自己的策略信息不能使用更新创建，需要调整
+		// 保存自己的策略信息
+		policyLid, err = service.PolicyLayout().Create(ctx, &entity.PolicyLayout{
+			Info: req.PolicyInfo,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	err = service.Layout().Update(ctx, &entity.Layout{
+		Id:         int(req.Id),
+		LocationId: int(req.LocationId),
+		PolicyCId:  int(req.PolicyCId),
+		PolicyLId:  policyLid,
+		LayoutName: req.LayoutName,
+		Map:        req.Map,
+		Status:     int(req.Status),
+		Sort:       int(req.Sort),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return
 }
 
 func (*Controller) Delete(ctx context.Context, req *v1.DeleteReq) (res *v1.DeleteRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	err = service.Layout().Delete(ctx, int(req.Id))
+	if err != nil {
+		return nil, err
+	}
+	return
 }
